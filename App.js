c26b4fdf1fcd13746f709399af56677d3d06cb3d
Daniel Matta <dinkito9@gmail.com>
2023-04-01 18:40:57 +0200

    add map

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './screens/HomeScreen';
+import { LatLng, LeafletView } from 'react-native-leaflet-view';
 
 const Stack = createNativeStackNavigator();
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import { View, Text} from 'react-native'
 import React from 'react'
-
+import MapView from 'react-native-maps'
+import {PROVIDER_GOOGLE} from 'react-native-maps'
 const HomeScreen = () => {
   return (
-    <View className="bg-white min-h-screen items-center justify-center">
-        <Text>Hello</Text>
+    
+    <View className="bg-white min-h-screen items-center justify-center ">
+         <MapView style={{height: '50%', width: '100%'}}
+         provider={PROVIDER_GOOGLE} /> 
     </View>
   )
 }
