6b1f103ff7e309aa9d2dc79c6e31a8bc175c76d9
sevcak <sevclukas0@gmail.com>
2023-04-01 14:23:57 +0200

    add Nativewind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text} from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
+    <View className="bg-white min-h-screen items-center justify-center">
         <Text>Hello</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
 
 export default HomeScreen;
